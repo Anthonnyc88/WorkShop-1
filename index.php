7ee50a04f0fc4e03a4a68af1f4728d26602885fd
punts hola mundo <anthonnyc10@gmail.com>
2018-09-18 20:16:53 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nuestro Primer Programa</title>
+</head>
+<body>
+	 <center>
+		<h1><?php echo "Hello World" ?></h1>
+<?php 
+
+echo "<td><a href='Hola.jpg'>";
+echo "<img src='Hola.jpg' width=600 height=400</img> ";
+
+ ?>
+ </center>
+</body>
+</html>  
